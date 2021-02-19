1bb97c7d21a411dc0c884237066cbc21991c2011
Felipe <felipemfmayer@gmail.com>
2021-02-19 00:33:30 -0300

    test_get_organization fixed

--- vough_backend/api/tests/tests_views.py
+++ vough_backend/api/tests/tests_views.py
@@ -1,39 +1,28 @@
 from rest_framework.test import APIRequestFactory, APITestCase
 from rest_framework.reverse import reverse
 from rest_framework import status
+from django.core import serializers
 from api.models import Organization
 import os
 
 # Create your tests here.
 
-class AccountTests(APITestCase):
+class OrganizationViewSetTest(APITestCase):
     def test_get_organization(self):
         """
-        
+        Teste se o status code retorna 200 e a organização é adicionada
+        no banco de dados
         """
         data = {'instruct-br', }
         url = reverse('api:orgs-detail', data)        
         response = self.client.get(url, format='json')
+        org = Organization.objects.filter(login='instruct-br').first()
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data, {'login': 'instruct-br',
-                                         'name': 'Instruct',
-                                          'score': 50}) # Esse score pode mudar.. verificar como deixar isso apenas como int
+        self.assertEqual(response.data, {'login': org.login,
+                                        'name': org.name,
+                                        'score': org.score })
 
-# Commnet line CTRL K C , CTRL K U 
-    # def test_retrieve_organization(self):
-    #     """
-    #     """
-    #     data =  {'instruct-br', }
-    #     url = reverse('api:orgs-detail', data)  
-    #     urllist = reverse('api:orgs-list')  
-    #     response = self.client.get(url, format='json')
-    #     responselist = self.client.get(urllist, format='json')
-    #     orgs = Organization.objects.filter(
-    #         login = 'instruct-br',
-    #         name = 'Instruct'
-    #     ).exists()
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-    #     self.assertTrue(orgs)
+
 
     def test_retrieve_organization(self):
         """
